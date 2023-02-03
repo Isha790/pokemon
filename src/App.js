5267f67a707a12614dbebd2bbd53ba2c2d5e490a
Isha Khatri <khatriisha79@gmail.com>
2023-02-03 14:43:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonSearch from "./components/PokemonSearch";
+import PokemonTable from "./components/PokemonTable";
 
 function App() {
+  const [filter, setFilter] = useState("");
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("pokemon.json")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1em",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "80% 20%",
+          gridColumnGap: "1em",
+        }}
+      >
+        <div>
+          <PokemonSearch filter={filter} setFilter={setFilter} />
+          <PokemonTable
+            pokemon={pokemon}
+            filter={filter}
+            setSelectedPokemon={setSelectedPokemon}
+          />
+        </div>
+        {selectedPokemon && <PokemonInfo {...selectedPokemon} />}
+      </div>
     </div>
   );
 }
